dff5221c6651380425f0991e4e12e85571136b8f
renbz <1362206371@qq.com>
2021-07-19 21:19:04 +0800

    test

---
+++ R06_NetWork_NIO_NIO_AIO/src/main/java/com/ren/BIO_ChatRoom/client/AChatClient.java
@@ -0,0 +1,12 @@
+package com.ren.BIO_ChatRoom.client;
+
+/**
+ * @author Ren
+ */
+
+public class AChatClient {
+    public static void main(String[] args) {
+        ChatClient chatClient = new ChatClient();
+        chatClient.start();
+    }
+}
---
+++ R06_NetWork_NIO_NIO_AIO/src/main/java/com/ren/BIO_ChatRoom/client/BChatClient.java
@@ -0,0 +1,12 @@
+package com.ren.BIO_ChatRoom.client;
+
+/**
+ * @author Ren
+ */
+
+public class BChatClient {
+    public static void main(String[] args) {
+        ChatClient chatClient = new ChatClient();
+        chatClient.start();
+    }
+}
